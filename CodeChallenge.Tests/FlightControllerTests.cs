e534f2f4cf4853650d7f34450ba5ca440bcb48d3
James Jackson <jjaxnjr@gmail.com>
2017-05-31 21:28:55 -0700

    Started building out GetFlights Tests and Action.

---
+++ CodeChallenge.Tests/FlightControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeChallenge.Controllers;
+using System.Web.Mvc;
+using CodeChallenge.Models;
+
+namespace CodeChallenge.Tests
+{
+    [TestClass]
+    public class FlightControllerTests
+    {
+        [TestMethod]
+        public void GetFlightsReturnsJsonResultObject()
+        {
+            // Arrange
+            var fieldController = new FieldController();
+            var fromAirport = "SEA";
+            var toAirport = "LAX";
+
+            // Act
+            var flights = fieldController.GetFlights(fromAirport, toAirport);
+
+            // Assert
+            Assert.IsInstanceOfType(flights, typeof(JsonResult));
+        }
+
+        [TestMethod]
+        public void GetFlightsWithSeaAndLaxReturnsFieldJsonObject()
+        {
+            // Arrange
+            var fieldController = new FieldController();
+            var fromAirport = "SEA";
+            var toAirport = "LAX";
+
+            // Act
+            var flights = fieldController.GetFlights(fromAirport, toAirport);
+
+            // Assert
+            Assert.IsInstanceOfType(flights.Data, typeof(Flight));
+        }
+    }
+}
---
+++ CodeChallenge/Models/Flight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeChallenge.Models
+{
+    public class Flight
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+
+    }
+}
