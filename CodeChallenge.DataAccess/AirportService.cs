890907f0f3541122765d635b5b069abedc8bf206
James Jackson <jjaxnjr@gmail.com>
2017-06-03 15:18:20 -0700

    AirportServiceTest GetAirportData() is passing again.

---
+++ CodeChallenge.DataAccess/AirportService.cs
@@ -0,0 +1,14 @@
+﻿using CodeChallenge.DataAccess.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.DataAccess
+{
+    public class AirportService : IAirportService
+    {
+
+    }
+}
