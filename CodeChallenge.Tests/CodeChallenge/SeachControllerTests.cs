ca8cfeb4457abd5c3f4eae19c788fad3ff9f56fa
James Jackson <jjaxnjr@gmail.com>
2017-06-03 14:51:21 -0700

    Added SearchControllerTests and testing for Index load.

---
+++ CodeChallenge.Tests/CodeChallenge/SeachControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CodeChallenge.Tests.CodeChallenge
+{
+    [TestClass]
+    public class SeachControllerTests
+    {
+        [TestMethod]
+        public void IndexReturnsView()
+        {
+            // Arrange
+            var searchController = new SearchController();
+
+            // Act
+            var results = searchController.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(results, typeof(ActionResult));
+        }
+    }
+}
