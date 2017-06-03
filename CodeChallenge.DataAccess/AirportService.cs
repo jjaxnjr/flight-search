9465f0c8535d9c7309c6f51af4f7139e94592271
James Jackson <jjaxnjr@gmail.com>
2017-06-03 15:23:19 -0700

    GetAirports now returning list of Airport Objects. Airport model was created earlier when flight model was created to save time.

--- CodeChallenge.DataAccess/AirportService.cs
+++ CodeChallenge.DataAccess/AirportService.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeChallenge.DataAccess.Models;
 
 namespace CodeChallenge.DataAccess
 {
     public class AirportService : IAirportService
     {
-
+        public IEnumerable<Airport> GetAirports()
+        {
+            return new List<Airport>();
+        }
     }
 }
--- CodeChallenge.DataAccess/Interfaces/IAirportService.cs
+++ CodeChallenge.DataAccess/Interfaces/IAirportService.cs
@@ -1,13 +1,10 @@
-﻿using System;
+﻿using CodeChallenge.DataAccess.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeChallenge.DataAccess.Interfaces
 {
     public interface IAirportService
     {
-        
+        IEnumerable<Airport> GetAirports();
     }
 }
