803f045a038f597e73895b3bacc6fb408a92053b
James Jackson <jjaxnjr@gmail.com>
2017-06-03 15:36:28 -0700

    Implemented the CsvHelper to load the csv into a list of airport objects. Set Csv to copy always.

--- CodeChallenge.DataAccess/AirportService.cs
+++ CodeChallenge.DataAccess/AirportService.cs
@@ -5,14 +5,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CodeChallenge.DataAccess.Models;
+using System.IO;
+using CsvHelper;
 
 namespace CodeChallenge.DataAccess
 {
     public class AirportService : IAirportService
     {
+        private string airportData;
+
+        private IEnumerable<Airport> _airports;
+        private IEnumerable<Airport> Airports
+        {
+            get
+            {
+                if (_airports == null)
+                {
+                    using (var stream = new StreamReader(airportData))
+                    {
+                        using (var csv = new CsvReader(stream))
+                        {
+                            _airports = csv.GetRecords<Airport>().ToList();
+                        }
+                    }
+                }
+                return _airports;
+            }
+        }
+
+        public AirportService()
+        {
+            airportData = Path.GetDirectoryName(AppDomain.CurrentDomain.SetupInformation.ApplicationBase) + @"\Debug\SampleData\Airports.csv";
+        }
+
+
         public IEnumerable<Airport> GetAirports()
         {
-            return new List<Airport>();
+            return Airports.ToList();
         }
     }
 }
