1cc1d1c88531e5a8f67d0cfdd9cfa822b4afc77e
James Jackson <jjaxnjr@gmail.com>
2017-06-03 15:14:49 -0700

    Started AirportServiceTests.

---
+++ CodeChallenge.Tests/CodeChallenge.DataAccess/AirportServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Tests.CodeChallenge.DataAccess
+{
+    [TestClass]
+    public class AirportServiceTests
+    {
+        [TestMethod]
+        public void GetAirportData()
+        {
+            // Arrange
+            var airportService = new AirportService();
+
+            // Act
+            
+
+            // Assert
+            
+        }
+    }
+}
