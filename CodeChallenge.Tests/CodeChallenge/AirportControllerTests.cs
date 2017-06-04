95328c288d8970ce47236529a44be3a6074b5e79
James Jackson <jjaxnjr@gmail.com>
2017-06-03 20:13:39 -0700

    Added AirportController Initial Test and implementation.

---
+++ CodeChallenge.Tests/CodeChallenge/AirportControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CodeChallenge.Controllers;
+using CodeChallenge.DataAccess.Interfaces;
+using CodeChallenge.DataAccess.Models;
+using CodeChallenge.Models;
+using CodeChallenge.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace CodeChallenge.Tests.CodeChallenge
+{
+    [TestClass]
+    public class AirportControllerTests
+    {
+        private Mock<IAirportService> airportServiceMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<Airport, AirportViewModel>());
+
+            airportServiceMock = new Mock<IAirportService>();
+            airportServiceMock.Setup(a => a.GetAirports()).Returns(SampleData.AirportData);
+        }
+
+        [TestMethod]
+        public void GetAirportsReturnsJsonResultObject()
+        {
+            // Arrange
+            var airportRepository = new AirportRepository(airportServiceMock.Object);
+            var airportController = new AirportController(airportRepository);
+
+            // Act
+            var results = airportController.GetAirports();
+
+            // Assert
+            Assert.IsInstanceOfType(results, typeof(JsonResult));
+        }
+    }
+}
---
+++ CodeChallenge/Controllers/AirportController.cs
@@ -0,0 +1,25 @@
+﻿using CodeChallenge.Interfaces;
+using CodeChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeChallenge.Controllers
+{
+    public class AirportController : Controller
+    {
+        private IAirportRepository airportRepository;
+
+        public AirportController(IAirportRepository aRepository)
+        {
+            airportRepository = aRepository;
+        }
+
+        public JsonResult GetAirports()
+        {
+            return Json(new List<AirportViewModel>(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
